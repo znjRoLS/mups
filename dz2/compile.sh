c8aced15b8298d133e1eedc1a52d714c9cd54545
rols <znjrols5@gmail.com>
2016-12-25 23:41:48 +0100

    compile fix

--- dz2/compile.sh
+++ dz2/compile.sh
@@ -1,5 +1,5 @@
-mpicc -lm -fopenmp -o bin/dz2z1 dz2z1.c
-mpicc -lm -fopenmp -o bin/dz2z2 dz2z2.c
-mpicc -lm -fopenmp -o bin/dz2z3 dz2z3.c
-mpicc -lm -fopenmp -o bin/dz2z4 dz2z4.c
-mpicc -lm -fopenmp -o bin/dz2z5 dz2z5.c
+mpicc -fopenmp -o bin/dz2z1 dz2z1.c -lm -w
+mpicc -fopenmp -o bin/dz2z2 dz2z2.c -lm -w
+mpicc -fopenmp -o bin/dz2z3 dz2z3.c -lm -w
+mpicc -fopenmp -o bin/dz2z4 dz2z4.c -lm -w
+mpicc -fopenmp -o bin/dz2z5 dz2z5.c -lm -w
